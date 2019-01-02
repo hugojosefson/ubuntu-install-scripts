ffcc918fb9b2323aa9cbe13c5f9f9f0546af9008
Hugo Josefson <hugo@josefson.org>
2019-01-02 21:11:19 +0100

    Make the tests pass in Docker.

--- sys/common.bash
+++ sys/common.bash
@@ -1,20 +1,29 @@
-function ensureInstalled {
-    while (( $# != 0 )); do
-        if ! isInstalled "$1"; then
-            sudo apt install -y "$1"
-        fi
-        shift
-    done
+export true=0
+export false=1
+
+function ensureInstalled() {
+  while (( $# != 0 )); do
+    if ! isInstalled "$1"; then
+      sudo env DEBIAN_FRONTEND=noninteractive apt-get install -y "$1"
+    fi
+    shift
+  done
 }
 
-function isInstalled {
-    if (( $# != 1 )); then
-      echo "isInstalled must be called with one argument" >&2
-      exit 1
-    fi
-    if [[ "$(dpkg-query --show -f '${status}' "$1")" == "install ok installed" ]] ; then
-        return 0
-    else
-        return 1
-    fi
+function isInstalled() {
+  if (( $# != 1 )); then
+    echo "isInstalled must be called with one argument" >&2
+    exit 1
+  fi
+  if [[ "$(dpkg-query --show -f '${status}' "$1" 2>/dev/null)" == "install ok installed" ]] ; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function insideDocker() {
+  [[ -e "/.dockerenv" ]] && return ${true}
+  [[ -e "/proc/self/cgroup" ]] && (grep docker /proc/self/cgroup >/dev/null) && return ${true}
+  return ${false}
 }
