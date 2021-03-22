07d0e3ff2ed4606b62f2ac84ffe3dd0243757437
Hugo Josefson <hugo@josefson.org>
2021-03-22 21:05:16 +0000

    Remove unused function.

--- lib/exec.ts
+++ lib/exec.ts
@@ -1,19 +1,3 @@
-export const withOutput = async (
-  cmd: Array<string>,
-): Promise<{ status: Deno.ProcessStatus; stdout: string; stderr: string }> => {
-  const process: Deno.Process = await Deno.run({
-    stdin: "null",
-    stdout: "piped",
-    stderr: "piped",
-    cmd,
-  });
-  return {
-    status: await process.status(),
-    stdout: new TextDecoder().decode(await process.output()),
-    stderr: new TextDecoder().decode(await process.stderrOutput()),
-  };
-};
-
 export const ensureSuccessful = async (
   cmd: Array<string>,
 ): Promise<void> => {
