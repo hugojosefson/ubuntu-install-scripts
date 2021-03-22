9e744bba35a3f0a49436a2b6ee44eb4f3b336a59
Hugo Josefson <hugo@josefson.org>
2021-03-22 17:51:28 +0000

    Move comment.

--- install-webstorm.ts
+++ install-webstorm.ts
@@ -2,14 +2,13 @@ import dropFile from "./lib/drop-file.ts";
 import { ensureAllOk } from "./lib/ensure-ok.ts";
 import ensureLineInFile from "./lib/ensure-line-in-file.ts";
 
-// Not installing package locally. Using webstorm via isolate-in-docker.
 export default (): Promise<void> => {
   const ws = `#!/usr/bin/env bash
 arg=\${1:-.}
 webstorm "\${arg}" &>/dev/null &
 `;
   return ensureAllOk([
-    // ensureInstalledFlatpak("com.jetbrains.WebStorm"),
+    // ensureInstalledFlatpak("com.jetbrains.WebStorm"), // Not installing package locally. Using webstorm via isolate-in-docker.
     dropFile(ws)("~/bin/ws", 0o775),
     ensureLineInFile('export PATH="~/bin:$PATH"')("~/.bashrc"),
   ]);
