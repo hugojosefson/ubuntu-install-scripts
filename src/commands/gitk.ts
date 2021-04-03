c751ac71fbf91e43a8e7b261d5ecee878633d8a3
Hugo Josefson <hugo@josefson.org>
2021-04-03 17:12:15 +0000

    Extract gitk.

---
+++ src/commands/gitk.ts
@@ -0,0 +1,16 @@
+import { Command } from "../model/command.ts";
+import { DropExecutable, LineInFile } from "./common/file-commands.ts";
+import { OsPackage } from "./common/os-package.ts";
+import { ParallelCommand } from "./common/parallel-command.ts";
+
+export const gitk: Command = new ParallelCommand(
+  new OsPackage("git"),
+  new DropExecutable(
+    "~/bin/gk",
+    `#!/usr/bin/env bash
+arg=\${1:---all}
+gitk "\${arg}" &>/dev/null &
+`,
+  ),
+  new LineInFile("~/.bashrc", 'export PATH="~/bin:$PATH"'),
+);
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,23 +1,6 @@
-import {
-  Command,
-
-
-
-} from "../model/command.ts";
-import {DropExecutable, LineInFile} from "./common/file-commands.ts";
+import { Command } from "../model/command.ts";
 import { OsPackage } from "./common/os-package.ts";
-import {ParallelCommand} from "./common/parallel-command.ts";
 
 export const awscli: Command = new OsPackage("aws-cli");
 export const brave: Command = new OsPackage("brave");
-export const gitk: Command = new ParallelCommand(
-  new OsPackage("git"),
-  new DropExecutable(
-    "~/bin/gk",
-    `#!/usr/bin/env bash
-arg=\${1:---all}
-gitk "\${arg}" &>/dev/null &
-`,
-  ),
-  new LineInFile("~/.bashrc", 'export PATH="~/bin:$PATH"'),
-);
+export { gitk } from "./gitk.ts";
