cd851a49b54f2ea4ac4c5361e8224bacfc880103
Hugo Josefson <hugo@josefson.org>
2021-04-05 10:12:07 +0000

    vim.

--- install-vim.ts
+++
@@ -1,9 +0,0 @@
-import { ensureInstalled } from "./lib/installer.ts";
-import ensureLineInFile from "./lib/ensure-line-in-file.ts";
-import { ensureAllOk } from "./lib/ensure-ok.ts";
-
-export default (): Promise<void> =>
-  ensureAllOk([
-    ensureInstalled(["vim"]),
-    ensureLineInFile("EDITOR=vim")("/etc/environment"),
-  ]);
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,9 +1,11 @@
 import { Command } from "../model/command.ts";
 import { OsPackage } from "./common/os-package.ts";
+import { vim } from "./vim.ts";
 import { gitk } from "./gitk.ts";
 
 const commands: Record<string, Command> = {
   gitk,
+  vim,
   awscli: new OsPackage("aws-cli"),
   libreoffice: OsPackage.multi([
     "libreoffice-fresh",
