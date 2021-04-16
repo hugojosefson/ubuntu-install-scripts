3eabcf08fcb31815a5fb33e680272020bd14fe0b
Hugo Josefson <hugo@josefson.org>
2021-04-16 15:00:06 +0000

    Upgrade deps.

--- src/deps.ts
+++ src/deps.ts
@@ -1,13 +1,17 @@
-import { dirname } from "https://deno.land/std@0.91.0/path/mod.ts";
+import { dirname } from "https://deno.land/std@0.93.0/path/mod.ts";
 export { dirname };
 
-import { exists } from "https://deno.land/std@0.91.0/fs/mod.ts";
+import { exists } from "https://deno.land/std@0.93.0/fs/mod.ts";
 export { exists };
 
 import debounce from "https://cdn.skypack.dev/lodash.debounce@v4.0.8";
 export { debounce };
 
-import { error, success, warning } from "https://deno.land/x/colorlog/mod.ts";
+import {
+  error,
+  success,
+  warning,
+} from "https://deno.land/x/colorlog@v1.0/mod.ts";
 type LogColorer = (val: any) => string;
 export const colorlog: {
   error: LogColorer;
