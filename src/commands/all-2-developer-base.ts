f76fa553fe8f6ec0c62ec8dc7645644fc34d37a6
Hugo Josefson <hugo@josefson.org>
2021-05-16 09:59:04 +0000

    Replace bash-git-prompt with starship, in all-2-developer-base.

--- src/commands/all-2-developer-base.ts
+++ src/commands/all-2-developer-base.ts
@@ -1,11 +1,11 @@
 import { Command } from "../model/command.ts";
 import { all1MinimalSanity } from "./all-1-minimal-sanity.ts";
-import { bashGitPrompt } from "./bash-git-prompt.ts";
 import { InstallAurPackage, InstallOsPackage } from "./common/os-package.ts";
 import { docker } from "./docker.ts";
 import { gitk } from "./gitk.ts";
 import { isolateInDocker } from "./isolate-in-docker.ts";
 import { meld } from "./meld.ts";
+import { starship } from "./starship.ts";
 import { virtManager } from "./virt-manager.ts";
 
 export const all2DeveloperBase = Command.custom()
@@ -22,10 +22,10 @@ export const all2DeveloperBase = Command.custom()
       "git-revise",
       "mdr",
     ].map(InstallAurPackage.of),
-    bashGitPrompt,
     docker,
     gitk,
     isolateInDocker,
     meld,
+    starship,
     virtManager,
   ]);
