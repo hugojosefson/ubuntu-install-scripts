51348850e3125f6a0315acc57520815538fffb3d
Hugo Josefson <hugo@josefson.org>
2021-05-16 09:35:47 +0000

    Activate starship in .bashrc.

--- src/commands/starship.ts
+++ src/commands/starship.ts
@@ -2,28 +2,39 @@ import { Command } from "../model/command.ts";
 import { FileSystemPath } from "../model/dependency.ts";
 import { readRelativeFile } from "../os/read-relative-file.ts";
 import { targetUser } from "../os/user/target-user.ts";
-import { CreateFile } from "./common/file-commands.ts";
+import { CreateFile, LineInFile } from "./common/file-commands.ts";
 import { InstallOsPackage } from "./common/os-package.ts";
 import { Exec } from "./exec.ts";
 import { vim } from "./vim.ts";
 
+const installStarship = new Exec([], [], targetUser, {}, [
+  "sh",
+  "-c",
+  "curl -fsSL https://starship.rs/install.sh | sh -s -- --yes",
+])
+  .withDependencies(
+    [
+      InstallOsPackage.of("noto-fonts-emoji"),
+      InstallOsPackage.of("powerline-fonts"),
+      vim,
+    ],
+  );
+
+const activateStarship = new LineInFile(
+  targetUser,
+  FileSystemPath.of(targetUser, "~/.bashrc"),
+  'eval "$(starship init bash)"',
+);
+
+const configureStarship = new CreateFile(
+  targetUser,
+  FileSystemPath.of(targetUser, "~/.config/starship.toml"),
+  await readRelativeFile("./files/starship.toml", import.meta.url),
+);
+
 export const starship = Command.custom()
   .withDependencies([
-    new Exec([], [], targetUser, {}, [
-      "sh",
-      "-c",
-      "curl -fsSL https://starship.rs/install.sh | sh -s --yes",
-    ])
-      .withDependencies(
-        [
-          InstallOsPackage.of("noto-fonts-emoji"),
-          InstallOsPackage.of("powerline-fonts"),
-          vim,
-        ],
-      ),
-    new CreateFile(
-      targetUser,
-      FileSystemPath.of(targetUser, "~/.config/starship.toml"),
-      await readRelativeFile("./files/starship.toml", import.meta.url),
-    ),
+    installStarship,
+    activateStarship,
+    configureStarship,
   ]);
