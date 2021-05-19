eafcf9d510c36cb8a34fd29c40e4af8c2c1b1634
Hugo Josefson <hugo@josefson.org>
2021-05-19 21:08:24 +0000

    Correct ~/bin PATH.

--- src/commands/add-home-bin-to-path.ts
+++ src/commands/add-home-bin-to-path.ts
@@ -3,12 +3,14 @@ import { FileSystemPath } from "../model/dependency.ts";
 import { targetUser } from "../os/user/target-user.ts";
 import { CreateDir, LineInFile } from "./common/file-commands.ts";
 
+const HOME_BIN = FileSystemPath.of(targetUser, "~/bin");
+
 export const addHomeBinToPath = Command.custom()
   .withDependencies([
-    new CreateDir(targetUser, FileSystemPath.of(targetUser, "~/bin")),
+    new CreateDir(targetUser, HOME_BIN),
     new LineInFile(
       targetUser,
       FileSystemPath.of(targetUser, "~/.bashrc"),
-      'export PATH="~/bin:$PATH"',
+      `export PATH="${HOME_BIN.path}:$PATH"`,
     ),
   ]);
