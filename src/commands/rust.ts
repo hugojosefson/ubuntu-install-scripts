8250c8426488ba8b10f1d5bc769e45eb2b00bb25
Hugo Josefson <hugo@josefson.org>
2021-05-22 23:31:56 +0200

    Fix rust.

--- src/commands/rust.ts
+++ src/commands/rust.ts
@@ -1,12 +1,7 @@
 import { Command } from "../model/command.ts";
-import { FileSystemPath } from "../model/dependency.ts";
 import { targetUser } from "../os/user/target-user.ts";
-import { LineInFile } from "./common/file-commands.ts";
 import { InstallOsPackage } from "./common/os-package.ts";
 import { Exec } from "./exec.ts";
-
-const split = (separator: string | RegExp) =>
-  (a: string): Array<string> => a.split(separator);
 
 export const rust = Command.custom()
   .withDependencies([
@@ -14,14 +9,10 @@ export const rust = Command.custom()
       targetUser,
       {},
       [
-        "rustup toolchain install stable",
-        "rustup default stable",
-      ].map(split(" ")),
+        ["sh", "-c", "curl -fsSL https://sh.rustup.rs | sh -s -- -y"],
+        // ["rustup", "toolchain", "install", "stable"],
+        // ["rustup", "default", "stable"],
+      ],
     ))
-      .withDependencies(["base-devel", "rustup"].map(InstallOsPackage.of)),
-    new LineInFile(
-      targetUser,
-      FileSystemPath.of(targetUser, "~/.bashrc"),
-      'export PATH="~/.cargo/bin:$PATH"',
-    ),
+      .withDependencies(["base-devel"].map(InstallOsPackage.of)),
   ]);
