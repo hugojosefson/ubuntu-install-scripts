327774228d9e81b303e5183d47fc66417a1c9df3
Hugo Josefson <hugo@josefson.org>
2021-05-22 23:36:14 +0200

    Simplify rust.

--- src/commands/rust.ts
+++ src/commands/rust.ts
@@ -1,18 +1,11 @@
-import { Command } from "../model/command.ts";
 import { targetUser } from "../os/user/target-user.ts";
 import { InstallOsPackage } from "./common/os-package.ts";
 import { Exec } from "./exec.ts";
 
-export const rust = Command.custom()
-  .withDependencies([
-    (Exec.sequentialExec(
-      targetUser,
-      {},
-      [
-        ["sh", "-c", "curl -fsSL https://sh.rustup.rs | sh -s -- -y"],
-        // ["rustup", "toolchain", "install", "stable"],
-        // ["rustup", "default", "stable"],
-      ],
-    ))
-      .withDependencies(["base-devel"].map(InstallOsPackage.of)),
-  ]);
+export const rust = new Exec(
+  [InstallOsPackage.of("base-devel")],
+  [],
+  targetUser,
+  {},
+  ["sh", "-c", "curl -fsSL https://sh.rustup.rs | sh -s -- -y"],
+);
