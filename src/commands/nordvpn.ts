b17b6af7178999fcbe966a7966821034b230ebd7
Hugo Josefson <hugo@josefson.org>
2021-05-31 21:48:44 +0200

    TODO nordvpn.

--- src/commands/nordvpn.ts
+++ src/commands/nordvpn.ts
@@ -2,6 +2,8 @@ import { ROOT, targetUser } from "../os/user/target-user.ts";
 import { InstallAurPackage } from "./common/os-package.ts";
 import { Exec } from "./exec.ts";
 
+// TODO: sudo systemctl enable --now nordvpnd
+
 export const nordvpn = new Exec(
   [InstallAurPackage.of("nordvpn-bin")],
   [],
