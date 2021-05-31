6c714fe87d3827a64f6a726182f7b9c6f0fc5a24
Hugo Josefson <hugo@josefson.org>
2021-05-31 22:13:54 +0200

    Enable yubikey.

--- src/commands/yubikey.ts
+++ src/commands/yubikey.ts
@@ -1,11 +1,15 @@
 import { Command } from "../model/command.ts";
+import { ROOT } from "../os/user/target-user.ts";
 import { InstallOsPackage } from "./common/os-package.ts";
+import { Exec } from "./exec.ts";
 
-export const yubikey: Command = Command
-  .custom()
+export const yubikey: Command = Exec.sequentialExec(ROOT, {}, [
+  "sudo systemctl enable --now pcscd.service".split(" "),
+])
   .withDependencies([
     "yubikey-personalization-gui",
     "yubioath-desktop",
     "yubikey-manager",
     "yubikey-manager-qt",
+    "ccid",
   ].map(InstallOsPackage.of));
