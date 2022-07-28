7cb83b8f3ebf8991a50bfc01735c6b58649f4807
Hugo Josefson <hugo@josefson.org>
2022-07-28 23:07:56 +0200

    virtualbox

--- src/commands/virtualbox.ts
+++ src/commands/virtualbox.ts
@@ -1,10 +1,28 @@
 import { Command } from "../model/command.ts";
-import { InstallBrewPackage, InstallOsPackage } from "./common/os-package.ts";
+import { InstallOsPackage } from "./common/os-package.ts";
+import { Exec } from "./exec.ts";
+import { ROOT } from "../os/user/target-user.ts";
+
+/** pre-register accept license for deb package virtualbox-ext-pack */
+const acceptLicense = new Exec(
+  [],
+  [],
+  ROOT,
+  {},
+  [
+    "sh",
+    "-c",
+    "echo virtualbox-ext-pack virtualbox-ext-pack/license select true | debconf-set-selections",
+  ],
+);
 
 export const virtualbox: Command = Command
   .custom()
   .withDependencies([
     InstallOsPackage.of("virtualbox"),
-    InstallOsPackage.of("virtualbox-guest-iso"),
-    InstallBrewPackage.of("virtualbox-ext-oracle"),
+    InstallOsPackage.of("virtualbox-guest-additions-iso"),
+    InstallOsPackage.of("virtualbox-ext-pack").withDependencies([
+      acceptLicense,
+    ]),
+    InstallOsPackage.of("virtualbox-qt"),
   ]);
