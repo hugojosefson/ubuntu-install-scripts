8d6b67a49d75efff26645af7b0c184d3e06622c4
Hugo Josefson <hugo@josefson.org>
2022-07-31 14:13:28 +0000

    feat(vim): update-alternatives editor

--- src/commands/vim.ts
+++ src/commands/vim.ts
@@ -3,10 +3,21 @@ import { FileSystemPath } from "../model/dependency.ts";
 import { ROOT } from "../os/user/target-user.ts";
 import { LineInFile } from "./common/file-commands.ts";
 import { ReplaceOsPackage } from "./common/os-package.ts";
+import { Exec } from "./exec.ts";
 
 export const vim: Command = Command.custom()
   .withDependencies([
-    ReplaceOsPackage.of2("vim-runtime", "neovim"),
+    new Exec(
+      [ReplaceOsPackage.of2("vim-runtime", "neovim")],
+      [FileSystemPath.of(ROOT, "/etc/alternatives")],
+      ROOT,
+      {},
+      [
+        "bash",
+        "-c",
+        'sudo update-alternatives --set editor "$(readlink -f "$(command -v vim)")"',
+      ],
+    ),
     new LineInFile(
       ROOT,
       FileSystemPath.of(ROOT, "/etc/environment"),
