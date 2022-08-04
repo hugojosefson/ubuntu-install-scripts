0d8a38933f2242c651ac0aca9108811232cf90f3
Hugo Josefson <hugo@josefson.org>
2022-08-04 16:34:22 +0200

    feat: add open-mux to .bash_history

--- src/commands/open-mux.ts
+++ src/commands/open-mux.ts
@@ -2,15 +2,26 @@ import { Command } from "../model/command.ts";
 import { FileSystemPath } from "../model/dependency.ts";
 import { readRelativeFile } from "../os/read-relative-file.ts";
 import { targetUser } from "../os/user/target-user.ts";
-import { CreateFile, MODE_EXECUTABLE_775 } from "./common/file-commands.ts";
+import {
+  CreateFile,
+  LineInFile,
+  MODE_EXECUTABLE_775,
+} from "./common/file-commands.ts";
+import { addHomeBinToPath } from "./add-home-bin-to-path.ts";
 
 export const openMux = Command.custom()
   .withDependencies([
+    addHomeBinToPath,
     new CreateFile(
       targetUser,
       FileSystemPath.of(targetUser, "~/bin/open-mux"),
       await readRelativeFile("./files/open-mux", import.meta.url),
       false,
       MODE_EXECUTABLE_775,
+    ),
+    new LineInFile(
+      targetUser,
+      FileSystemPath.of(targetUser, "~/.bash_history"),
+      "open-mux",
     ),
   ]);
