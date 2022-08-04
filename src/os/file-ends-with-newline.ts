3ad0836aa9dc3166cd960856aae50bb1056ff9a5
Hugo Josefson <hugo@josefson.org>
2022-08-04 16:48:07 +0200

    fix: accept non-existing file

--- src/os/file-ends-with-newline.ts
+++ src/os/file-ends-with-newline.ts
@@ -3,6 +3,12 @@ import { FileSystemPath } from "../model/dependency.ts";
 export async function fileEndsWithNewline(
   file: FileSystemPath,
 ): Promise<boolean> {
-  const data = await Deno.readFile(file.path);
-  return data.slice(-1)[0] === 10;
+  try {
+    const data = await Deno.readFile(file.path);
+    return data.slice(-1)[0] === 10;
+  } catch (e) {
+    if (e instanceof Deno.errors.NotFound) {
+      return true;
+    }
+  }
 }
